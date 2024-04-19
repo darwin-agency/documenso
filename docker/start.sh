8baafd6faa851991df1cf5602f8d09a9380ad399
Lucas Alberto <lucas.alberto98@gmail.com>
2024-04-19 11:44:24 -0300

    fix: fix command start

--- docker/start.sh
+++ docker/start.sh
@@ -3,5 +3,6 @@
 set -x
 
 npx prisma migrate deploy --schema ./packages/prisma/schema.prisma
-npx playwright install
+npm install -D @playwright/test@latest
+npx playwright install --with-deps
 node apps/web/server.js
